cdb5f1e5c8cba5f6858f17855f907b5b83b24f99
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-10-01 23:26:26 +0530

    updated frontend access

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const bookRoutes = require('./routes/bookRoutes'); // Import the routes
+const path = require('path'); // Add this line
 
 const app = express();
 const port = 3000;
@@ -16,6 +17,8 @@ app.use(bodyParser.json());
 
 // Middleware to parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'kdeditor')));
 
 // Use the book routes
 app.use('/', bookRoutes);
